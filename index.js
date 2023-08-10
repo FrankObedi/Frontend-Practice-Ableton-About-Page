cd96fc69fe954ebf278a31567ffa3b86f76a7147
Frank Obedi <frankmobedi@gmail.com>
2023-08-10 19:10:21 -0400

    completed website nav

--- index.js
+++ index.js
@@ -243,37 +243,45 @@ const handleCollapsible = () => {
 
 // tablet and mobile menu toggle
 const menu = document.querySelector(".main-nav-content");
-const secondaryNav = document.querySelector(".secondary-nav");
 const menuToggle = document.querySelector(".menu-toggle");
 const menuToggleIcon = document.querySelector(".bx-caret-down");
 const logo = document.querySelector(".main-site-logo");
-
+const secondaryNav = document.querySelector(".secondary-nav");
+let isNavOpen = false;
 // function to toggle menu
 const setOpenNav = () => {
+  isNavOpen = !isNavOpen;
+  if (isNavOpen === false) {
+    secondaryNav.style.position = "relative";
+  }
   logo.classList.toggle("white-logo");
   menuToggle.classList.toggle("white-menu-toggle");
   menuToggleIcon.classList.toggle("bx-caret-up");
   menu.classList.toggle("visible-nav");
-  secondaryNav.classList.toggle("sticky");
 };
 
 // Make secondary nav sticky when scrolling up
 let lastScroll = 0;
 window.addEventListener("scroll", function () {
   const currentScrollPos = window.scrollY;
+
   if (currentScrollPos > lastScroll) {
     secondaryNav.style.top = "-140px";
-  }
-
-  if (window.matchMedia("(max-width: 1024px)").matches) {
-  } else {
+  } else if (currentScrollPos < lastScroll && currentScrollPos > 45) {
     secondaryNav.style.top = "0";
     secondaryNav.style.position = "sticky";
+
     secondaryNav.style.zIndex = "15";
+  } else {
+    secondaryNav.style.zIndex = "1";
   }
-  // if (currentScrollPos <= 0) {
-  //   secondaryNav.style.zIndex = "1";
-  //   console.log(lastScroll);
-  // }
+
+  if (isNavOpen) {
+    secondaryNav.style.background = "#ffffffe1";
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+      secondaryNav.style.position = "fixed";
+    }
+  }
+
   lastScroll = currentScrollPos;
 });
